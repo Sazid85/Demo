d62ba1dd757f26241193c49a547abe62e0325f58
Nazeeruddin <learnerlandmark@gmail.com>
2024-02-16 08:34:08 +0530

    Addition Logic Added

---
+++ Demo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+    return a+b;
+}
+int main()
+{
+    std::cout<<"Hello"<<std::endl;
+    cout<<add(10,20)<<endl;
+    return 0;
+}
